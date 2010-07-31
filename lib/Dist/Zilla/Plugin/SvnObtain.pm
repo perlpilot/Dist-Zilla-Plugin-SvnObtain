b3bdfe2c829c923b64e8fc05a7d6190e1ff80874
Jonathan Scott Duff <duff@pobox.com>
2010-07-31 13:22:02 -0500

    Added some documentation

--- lib/Dist/Zilla/Plugin/SvnObtain.pm
+++ lib/Dist/Zilla/Plugin/SvnObtain.pm
@@ -89,6 +89,57 @@ In your F<dist.ini>:
   [SvnObtain]
     ;subdir = url                                   revision
     parrot  = https://svn.parrot.org/parrot/trunk   48152
-  [SvnObtain/
+
+  [SvnObtain/path/to/some/other/dir]
+    blah = http://svn.example.com/repos/my-project
 
 =head1 DESCRIPTION
+
+Uses L<SVN::Client> to obtain files from a subversion repository for
+inclusion in a distribution made with L<Dist::Zilla>.
+
+C<[SvnObtain]> sections in your F<dist.ini> file describe a set of
+Subversion repositories that will be downloaded into the current
+directory prior to building a distribution. Subdirectories will be
+created that correspond to the name of the projects listed in that
+section. Optionally, after the URL of the subversion repository, you may
+specify a particular revision to check out. If you do not specify a
+revision, C<HEAD> will be used. For instance, to include a copy
+MIT's simile timeline widget into your distribution, your
+F<dist.ini> would contain something like this:
+
+  [SvnObtain]
+    simile = http://simile-widgets.googlecode.com/svn
+
+This would create a subdirectory called F<simile> in the current
+directory that contains the C<HEAD> revision.
+
+If you do not wish the project directories to be created in the current
+directory, you may specify a path relative to the current directory as
+part of the section name. For instance, to checkout subversion
+repositories into a subdirectory called F<libs/javascript>, the section
+name would look like this:
+
+  [SvnObtain/libs/javascript]
+    jquery = http://jqueryjs.googlecode.com/svn/trunk
+    simile = http://simile-widgets.googlecode.com/svn  2100
+
+If the directory F<libs/javascript> does not exist, each component of
+the path will be created as necessary. Once the directory
+F<libs/javascript> exists, project directories will be created within it
+for F<jquery> and F<simile>. The F<jquery> checkout will be at the HEAD
+revision and the F<simile> checkout will be at revision 2100.
+
+=head1 AUTHOR
+
+Jonathan Scott Duff <duff@pobox.com>
+
+=head1 COPYRIGHT
+
+This software is copyright (c) 2010 by Jonathan Scott Duff
+
+This is free sofware; you can redistribute it and/or modify it under the
+same terms as the Perl 5 programming language itself.
+
+=cut
+
